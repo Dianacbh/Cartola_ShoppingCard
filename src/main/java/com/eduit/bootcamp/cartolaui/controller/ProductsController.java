5485cb5a40aa5f520281aebc29bf6e560b497801
diana <dianabasabe2503@gmail.com>
2023-03-30 16:45:16 -0300

    Frond-end

---
+++ src/main/java/com/eduit/bootcamp/cartolaui/controller/ProductsController.java
@@ -0,0 +1,38 @@
+package com.eduit.bootcamp.cartolaui.controller;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.eduit.bootcamp.cartolaui.model.UserLoginModel;
+
+@Controller
+@RequestMapping(path = "/products")
+public class ProductsController extends BaseController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ProductsController.class);
+	
+	public ProductsController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@GetMapping("")
+	public String mainPage(HttpServletRequest request, HttpServletResponse response, Model model) {
+		HttpSession session = request.getSession();
+		model.addAttribute("products", new ArrayList<ProductDTO>());
+		model.addAttribute("userLoginModel", new UserLoginModel());
+		checkUserSession(response, model, session);
+		
+		return "products";
+	}
+
+}
---
+++ src/test/java/com/eduit/bootcamp/cartolaui/CartUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduit.bootcamp.cartolaui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
